eaeaf41339ea75338bdc58dc04a8734048eccb6d
Muhammad Saad <dev.bysaad@gmail.com>
2025-09-02 11:35:42 +0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Wrapper from './Wrapper.jsx'
-
-createRoot(document.getElementById('root')).render(
-<Wrapper>
-    <h1>hello</h1>
-    <App/>
-</Wrapper>)
